1a4a10e5e5ea637d66f26e349d725527856f6c40
Catherine Salazar <catcreates13@gmail.com>
2018-10-29 23:19:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ end
   post "/login" do
     @user = User.find_by(:username => params[:username])
     if @user && @user.authenticate(params[:password])
-     session[:user_id] = user.id
+     session[:user_id] = @user.id
      redirect "/success"
    else
      redirect "/failure"
