3ed10f304032f408e3cc10aa8e366962f0006701
Catherine Salazar <catcreates13@gmail.com>
2018-10-29 23:13:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-    if user.username = ""
+    if params[:username]= "" || params[:password] = ""
       redirect "/failure"
-    elsif user.password = ""
-      redirect "/failure"
-  else user = user.username && user.password
+  else user = User.new(:username => params[:username], :password => params[:password])
     redirect "/login"
   end
 end
